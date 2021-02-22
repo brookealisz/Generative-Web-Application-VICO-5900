9c735b5c9694997dbc334edba46f2d995f7b8b7b
brookealisz <79415767+brookealisz@users.noreply.github.com>
2021-02-21 21:31:18 -0500

    Update p5.sound.js

--- p5.sound.js
+++ p5.sound.js
@@ -1223,33 +1223,33 @@ global.TONE_SILENCE_VERSION_LOGGING = true;
    *
    * <p>Some browsers require users to startAudioContext
    * with a user gesture, such as touchStarted in the example below.</p>
-   *
-   * @for p5
-   * @method getAudioContext
-   * @return {Object}    AudioContext for this sketch
-   * @example
-   * <div><code>
-   *  function draw() {
-   *    background(255);
-   *    textAlign(CENTER);
-   *
-   *    if (getAudioContext().state !== 'running') {
-   *      text('click to start audio', width/2, height/2);
-   *    } else {
-   *      text('audio is enabled', width/2, height/2);
-   *    }
-   *  }
-   *
-   *  function touchStarted() {
-   *    if (getAudioContext().state !== 'running') {
-   *      getAudioContext().resume();
-   *    }
-   *    var synth = new p5.MonoSynth();
-   *    synth.play('A4', 0.5, 0, 0.2);
-   *  }
-   *
-   * </div></code>
    */
+    @for p5
+    @method getAudioContext
+    @return {Object}    AudioContext for this sketch
+    @example
+    <div><code>
+     function draw() {
+       background(255);
+       textAlign(CENTER);
+   
+       if (getAudioContext().state !== 'running') {
+         text('click to start audio', width/2, height/2);
+       } else {
+         text('audio is enabled', width/2, height/2);
+       }
+     }
+   
+     function touchStarted() {
+       if (getAudioContext().state !== 'running') {
+         getAudioContext().resume();
+       }
+       var synth = new p5.MonoSynth();
+       synth.play('A4', 0.5, 0, 0.2);
+     }
+   
+    </div></code>
+   
 
   p5.prototype.getAudioContext = function () {
     return audiocontext;
@@ -1277,36 +1277,37 @@ global.TONE_SILENCE_VERSION_LOGGING = true;
    *  example below. This method utilizes
    *  <a href="https://github.com/tambien/StartAudioContext">StartAudioContext
    *  </a>, a library by Yotam Mann (MIT Licence, 2016).</p>
-   *  @param  {Element|Array}   [element(s)] This argument can be an Element,
-   *                                Selector String, NodeList, p5.Element,
-   *                                jQuery Element, or an Array of any of those.
-   *  @param  {Function} [callback] Callback to invoke when the AudioContext
-   *                                has started
-   *  @return {Promise}            Returns a Promise that resolves when
-   *                                       the AudioContext state is 'running'
-   *  @method userStartAudio
-   *  @for p5
-   *  @example
-   *  <div><code>
-   *  function setup() {
-   *    // mimics the autoplay policy
-   *    getAudioContext().suspend();
-   *
-   *    let mySynth = new p5.MonoSynth();
-   *
-   *    // This won't play until the context has resumed
-   *    mySynth.play('A6');
-   *  }
-   *  function draw() {
-   *    background(220);
-   *    textAlign(CENTER, CENTER);
-   *    text(getAudioContext().state, width/2, height/2);
-   *  }
-   *  function mousePressed() {
-   *    userStartAudio();
-   *  }
-   *  </code></div>
    */
+     @param  {Element|Array}   [element(s)] This argument can be an Element,
+                                   Selector String, NodeList, p5.Element,
+                                   jQuery Element, or an Array of any of those.
+     @param  {Function} [callback] Callback to invoke when the AudioContext
+                                   has started
+     @return {Promise}            Returns a Promise that resolves when
+                                          the AudioContext state is 'running'
+     @method userStartAudio
+     @for p5
+     @example
+     <div><code>
+     function setup() {
+       // mimics the autoplay policy
+       getAudioContext().suspend();
+   
+       let mySynth = new p5.MonoSynth();
+   
+       // This won't play until the context has resumed
+       mySynth.play('A6');
+     }
+     function draw() {
+       background(220);
+       textAlign(CENTER, CENTER);
+       text(getAudioContext().state, width/2, height/2);
+     }
+     function mousePressed() {
+       userStartAudio();
+     }
+     </code></div>
+   
 
 
   p5.prototype.userStartAudio = function (elements, callback) {
